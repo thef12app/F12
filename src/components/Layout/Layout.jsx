2e3e16ee11d6972549fa8d4d30f94d686a6f029c
Vigneshkumar Chinnachamy M <thegardenerwhocodes@gmail.com>
2021-10-05 17:10:27 +0530

    add rating

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,6 +2,8 @@ import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import styles from './Layout.module.scss';
 import { utilList as _utilList } from '../../pages/Utils/util-list';
 
+import ratingIcon from '../../assets/img/ratings.png';
+
 const utilList = _utilList.filter((u) => u.componentName);
 
 export const Layout = ({ children, actions, title }) => {
@@ -54,13 +56,25 @@ export const Layout = ({ children, actions, title }) => {
       </div>
       <div className={styles.content}>{children}</div>
       <div className={styles.footer}>
+        <a
+          href="https://chrome.google.com/webstore/detail/f12/mbnakamgdofpbfjpibdmcmjonhoncbgf/reviews"
+          target="_blank"
+          className={styles.footerLink}
+        >
+          Please rate us here
+          <img
+            src={ratingIcon}
+            alt="Rating Icon"
+            className={styles.ratingImage}
+          />
+        </a>
         <a href="https://www.buymeacoffee.com/thef12app" target="_blank">
           <img src="https://img.buymeacoffee.com/button-api/?text=Buy us a coffee&emoji=&slug=thef12app&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" />
         </a>
         <a
           href="https://thef12app.frill.co/b/n0o9qd06/feature-ideas"
           target="_blank"
-          className={styles.featureRequestLink}
+          className={styles.footerLink}
         >
           Request for a feature
         </a>

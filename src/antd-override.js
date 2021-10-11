4cf54151a5064658d58c4fd016bb0c68a4a2feb1
Aniket Aman <aniket.aman007@gmail.com>
2021-10-12 00:25:45 +0530

    minor styles for searchbox

--- src/antd-override.js
+++ src/antd-override.js
@@ -1,5 +1,7 @@
 const antdCustomTheme = {
-  'primary-color': 'teal',
+  'primary-color': '#4C8EAB',
+  'layout-sider-background': '#303030',
+  'menu-dark-bg': '#303030',
   'link-color': '#742d74eb',
   'border-radius-base': '4px',
   'font-family': `'Poppins',-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif`,
--- src/components/Layout/Sidemenu/index.tsx
+++ src/components/Layout/Sidemenu/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
-import { Layout, Menu, Typography } from 'antd';
+import { Input, Layout, Menu, Typography } from 'antd';
 import { utilList } from '../../../pages/Utils/util-list';
 import { useHistory } from 'react-router';
+
+import style from '../Layout.module.scss';
 
 const { Sider } = Layout;
 const SideMenu = () => {
   return (
-    <Sider>
+    <Sider width={'340px'}>
       <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+      <div style={{ width: '100%', padding: 10 }}>
+        <Input
+          placeholder="Search"
+          type="search"
+          style={{ padding: 10 }}
+          allowClear
+        ></Input>
+      </div>
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
         {utilList.map((li, index) => (
           <Menu.Item key={index}>
             <Typography.Link href={`#/${li.path}`}>{li.name}</Typography.Link>

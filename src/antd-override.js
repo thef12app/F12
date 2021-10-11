f5de9e57a3985243ce7068b9c90b1ef368355465
Aniket Aman <aniket.aman007@gmail.com>
2021-10-11 10:23:34 +0530

    added fallback system fonts

--- src/antd-override.js
+++ src/antd-override.js
@@ -2,7 +2,7 @@ const antdCustomTheme = {
   'primary-color': 'teal',
   'link-color': '#742d74eb',
   'border-radius-base': '4px',
-  'font-family': 'Poppins',
+  'font-family': `'Poppins',-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif`,
 };
 
 module.exports = antdCustomTheme;
